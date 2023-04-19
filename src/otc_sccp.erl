fceee7f82e301ee9a697459cd11a330a96025fe2
Mikael Lixenstrand <lixen@wgtwo.com>
2023-04-19 08:40:40 +0200

    fix order of PC and SSN

--- src/otc_sccp.erl
+++ src/otc_sccp.erl
@@ -1214,8 +1214,8 @@ encode_parameter(long_data, Bin) ->
 decode_address(<<NR:1, RI:1, GTI:4, SSNI:1, PCI:1, Bin0/binary>>) ->
     {PC, Bin1} = case PCI of
                      0 -> {undefined, Bin0};
-                     1 -> <<LSB:8, 0:2, MSB:6, Rest0/binary>> = Bin0,
-                          {<<MSB:6, LSB:8>>, Rest0}
+                     1 -> <<PCBin:2/binary, Rest0/binary>> = Bin0,
+                          {PCBin, Rest0}
                  end,
     {SSN, Bin2} = case SSNI of
                       0 -> {undefined, Bin1};
@@ -1353,7 +1353,7 @@ encode_address(#{national_use_indicator := NR,
              2#0000 ->
                  <<>>
          end,
-    <<NR:1, RI:1, GTI:4, SSNI:1, PCI:1, SSNBin/binary, PCBin/binary, GT/binary>>.
+    <<NR:1, RI:1, GTI:4, SSNI:1, PCI:1, PCBin/binary, SSNBin/binary, GT/binary>>.
 
 compose_encoding_scheme(unknown, _) ->
     2#0000;
